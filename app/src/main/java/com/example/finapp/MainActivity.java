64dacc07ea076e81db627b55bfedd11b9e813b3d
Guilherme Janke <guilhermejanke15@gmail.com>
2022-12-08 21:06:18 -0300

    Primeiro commit

---
+++ app/src/main/java/com/example/finapp/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.finapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void btnCadastroDeOperacoes_Click(View view){}
+
+    public void btnExtrato_Click(View view){}
+
+    public void btnPesquisar_Click(View view){}
+
+    public void btnListaClassificada_Click(View view){}
+
+    public void btnSair_Click(View view){}
+
+}
