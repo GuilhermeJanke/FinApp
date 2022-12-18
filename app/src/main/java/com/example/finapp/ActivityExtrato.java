199e253ebcb4695d4bc925bf2802d81dcb6777e8
Luis Felipe Fezer <luisfelipefezer@gmail.com>
2022-12-17 23:02:37 -0300

    Tentativa 1

--- app/src/main/java/com/example/finapp/ActivityExtrato.java
+++ app/src/main/java/com/example/finapp/ActivityExtrato.java
@@ -1,6 +1,8 @@
 package com.example.finapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
@@ -8,14 +10,27 @@ import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.TextView;
 
+import com.example.finapp.adapter.AdapterExtrato;
+
 public class ActivityExtrato extends AppCompatActivity {
+
+    private RecyclerView recyclerViewExtrato;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_extrato);
 
-        TextView saldo = findViewById(R.id.textViewExtract);
+        recyclerViewExtrato = findViewById(R.id.recyclerViewExtrato);
+
+        AdapterExtrato adapter = new AdapterExtrato();
+
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getApplicationContext());
+        recyclerViewExtrato.setLayoutManager(layoutManager);
+
+        recyclerViewExtrato.setHasFixedSize(true);
+
+        recyclerViewExtrato.setAdapter(adapter);
 
     }
 }
--- app/src/main/java/com/example/finapp/adapter/AdapterExtrato.java
+++ app/src/main/java/com/example/finapp/adapter/AdapterExtrato.java
@@ -1,6 +1,46 @@
 package com.example.finapp.adapter;
 
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.finapp.R;
+
 public class AdapterExtrato extends RecyclerView.Adapter<AdapterExtrato.MyViewHolder> {
+
+    public class MyViewHolder extends RecyclerView.ViewHolder{
+
+        TextView classificacao, data, valor;
+
+        public MyViewHolder(View view){
+            super(view);
+            classificacao = view.findViewById(R.id.textViewHeaderClassificacao);
+            data = view.findViewById(R.id.textViewHeaderData);
+            valor = view.findViewById(R.id.textViewHeaderValor);
+        }
+    }
+
+    @NonNull
+    @Override
+    public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View listItem = LayoutInflater.from(parent.getContext()).inflate(R.layout.activity_extrato, parent, false);
+        return new MyViewHolder(listItem);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
+        holder.classificacao.setText("Teste");
+        holder.data.setText("Teste2");
+        holder.valor.setText("Teste3");
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 1;
+    }
 }
