cd5ea5c8bf993590a706a801316df49e97d1e25f
Luis Felipe Fezer <luisfelipefezer@gmail.com>
2022-12-17 21:56:38 -0300

    Extrato inicial

---
+++ app/src/main/java/com/example/finapp/adapter/AdapterExtrato.java
@@ -0,0 +1,6 @@
+package com.example.finapp.adapter;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+public class AdapterExtrato extends RecyclerView.Adapter<AdapterExtrato.MyViewHolder> {
+}
