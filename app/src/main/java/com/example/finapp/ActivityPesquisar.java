31372887dc61c1d041ece470888c1564e779da41
Guilherme Janke <guilhermejanke15@gmail.com>
2022-12-19 20:29:01 -0300

    Pesquisar quase funcionando legal

--- app/src/main/java/com/example/finapp/ActivityPesquisar.java
+++ app/src/main/java/com/example/finapp/ActivityPesquisar.java
@@ -1,21 +1,16 @@
 package com.example.finapp;
-
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.DividerItemDecoration;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
-
 import android.os.Bundle;
 import android.view.View;
 import android.widget.LinearLayout;
 import android.widget.RadioButton;
-import android.widget.RadioGroup;
-
 import com.example.finapp.adapter.AdapterPesquisa;
 import com.example.finapp.database.Operation;
 import com.example.finapp.database.OperationsDAO;
 import com.santalu.maskara.widget.MaskEditText;
-
 import java.util.ArrayList;
 import java.util.List;
 
@@ -29,31 +24,37 @@ public class ActivityPesquisar extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pesquisar);
+        recyclerViewListaPesquisa = findViewById(R.id.recyclerViewPesquisar);
     }
 
     public void onClick(View view){
-        String dataInicial = String.valueOf(findViewById(R.id.dataInicial));
-        String dataFinal = String.valueOf(findViewById(R.id.dataFinal));
 
-        RadioGroup radioGroup = (RadioGroup)findViewById(R.id.rgPesquisar);
-        String radiovalue =((RadioButton)findViewById(radioGroup.getCheckedRadioButtonId())).getText().toString();
+        RadioButton radioButtonTodos = findViewById(R.id.todas);
+        RadioButton radioButtonCredito = findViewById(R.id.credito);
+        RadioButton radioButtonDebito = findViewById(R.id.debito);
 
-        this.createListaPesquisa(radiovalue);
+        if(radioButtonTodos.isChecked()) {
+            this.createListaPesquisa("todos");
+
+        }
+        else if(radioButtonCredito.isChecked()) {
+            this.createListaPesquisa("credito");
+        }
+        else if(radioButtonDebito.isChecked()) {
+            this.createListaPesquisa("debito");
+        }
+
         AdapterPesquisa adapterPesquisa = new AdapterPesquisa(listOperation);
 
-        RecyclerView.LayoutManager layoutManager =
-                new LinearLayoutManager(getApplicationContext());
-
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getApplicationContext());
         recyclerViewListaPesquisa.setLayoutManager(layoutManager);
+
         recyclerViewListaPesquisa.setHasFixedSize(true);
 
-        recyclerViewListaPesquisa.addItemDecoration(
-                new DividerItemDecoration(this, LinearLayout.VERTICAL));
-
+        recyclerViewListaPesquisa.addItemDecoration(new DividerItemDecoration(this, LinearLayout.VERTICAL));
         recyclerViewListaPesquisa.setAdapter(adapterPesquisa);
 
     }
-
     public void createListaPesquisa(String operacao) {
         operationDAO = new OperationsDAO(getApplicationContext());
         listOperation = operationDAO.listListaPesquisar(operacao);
--- app/src/main/java/com/example/finapp/database/OperationsDAO.java
+++ app/src/main/java/com/example/finapp/database/OperationsDAO.java
@@ -147,19 +147,58 @@ public class OperationsDAO {
     List<Operation> operationList = new ArrayList<>();
     String sql = "";
 
-    if(tipoOperacao == "Todas as Operações" ){
-      sql = "SELECT filter, type, SUM(value) as value FROM " + SimpleDBWrapper.TABLE_NAME_OPER + " GROUP BY type ORDER BY filter DESC;";
+    if(tipoOperacao == "todos" ){
+      sql = "SELECT filter, type, SUM(value) as value FROM " + SimpleDBWrapper.TABLE_NAME_OPER;
       Cursor cursor = read.rawQuery(sql,null);
+
+      while (cursor.moveToNext()) {
+        Operation operation = new Operation();
+        @SuppressLint("Range") String filter = cursor.getString(cursor.getColumnIndex("filter"));
+        @SuppressLint("Range") String type = cursor.getString(cursor.getColumnIndex("type"));
+        @SuppressLint("Range") double value = cursor.getDouble(cursor.getColumnIndex("value"));
+        operation.setFilter(filter);
+        operation.setType(type);
+        operation.setValue(value);
+        operationList.add(operation);
+      }
+
+      cursor.close();
       return operationList;
     }
-    if(tipoOperacao == "Crédito"){
+    if(tipoOperacao == "credito"){
       sql = "SELECT filter, type, SUM(value) as value FROM " + SimpleDBWrapper.TABLE_NAME_OPER + " WHERE filter = 'credito'";
       Cursor cursor = read.rawQuery(sql,null);
+
+      while (cursor.moveToNext()) {
+        Operation operation = new Operation();
+        @SuppressLint("Range") String filter = cursor.getString(cursor.getColumnIndex("filter"));
+        @SuppressLint("Range") String type = cursor.getString(cursor.getColumnIndex("type"));
+        @SuppressLint("Range") double value = cursor.getDouble(cursor.getColumnIndex("value"));
+        operation.setFilter(filter);
+        operation.setType(type);
+        operation.setValue(value);
+        operationList.add(operation);
+      }
+
+      cursor.close();
       return operationList;
     }
-    if(tipoOperacao == "Débito"){
+    if(tipoOperacao == "debito"){
       sql = "SELECT filter, type, SUM(value) as value FROM " + SimpleDBWrapper.TABLE_NAME_OPER + " WHERE filter = 'debito'";
       Cursor cursor = read.rawQuery(sql,null);
+
+      while (cursor.moveToNext()) {
+        Operation operation = new Operation();
+        @SuppressLint("Range") String filter = cursor.getString(cursor.getColumnIndex("filter"));
+        @SuppressLint("Range") String type = cursor.getString(cursor.getColumnIndex("type"));
+        @SuppressLint("Range") double value = cursor.getDouble(cursor.getColumnIndex("value"));
+        operation.setFilter(filter);
+        operation.setType(type);
+        operation.setValue(value);
+        operationList.add(operation);
+      }
+
+      cursor.close();
       return operationList;
     }
 
